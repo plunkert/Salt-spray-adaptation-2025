86ccfd19458e81633f51dcc0a6cc520ba56050aa
plunkert <169087226+plunkert@users.noreply.github.com>
2024-06-13 22:20:10 -0400

    Add boxplot and stats for contact angle

--- Scripts/contact_angle_data_wrangling.R
+++ Scripts/contact_angle_data_wrangling.R
@@ -82,3 +82,72 @@ contact_angle %>%
   aes(x=contact_angle) +
   facet_wrap(vars(surface_type), nrow = 2) +
   geom_boxplot()
+
+
+contact_ad_boxplot = contact_angle %>%
+  filter(!is.na(contact_angle) & leaf_side=="ad") %>%
+  ggplot() +
+  aes(x = ecotype, fill = ecotype, y = contact_angle) +
+  geom_boxplot(outliers = F) +
+  geom_jitter(position=position_jitter(0.1)) +
+  # Labels
+  scale_x_discrete(name = 'Ecotype') +
+  scale_y_continuous(
+    name = 'Contact Angle (degrees)', 
+  ) +
+  # Style
+  scale_fill_manual(values = c('#514663', '#cacf85')) +
+  theme_minimal() +
+  theme(
+    legend.position = "none",
+    axis.text = element_text(size=12),
+    axis.title = element_text(size=14),
+  )
+contact_ad_boxplot
+
+ggsave(
+  filename = 'Prelim_ad_contact_angle.png', 
+  plot = contact_ad_boxplot,
+  device = 'png',
+  path = './Results/Figures/',
+  scale = 1,
+  width = 3,
+  height = 6,
+  bg = 'white'
+)
+
+contact_ab_boxplot = contact_angle %>%
+  filter(!is.na(contact_angle) & leaf_side=="ab") %>%
+  ggplot() +
+  aes(x = ecotype, fill = ecotype, y = contact_angle) +
+  geom_boxplot(outliers = F) +
+  geom_jitter(position=position_jitter(0.1)) +
+  # Labels
+  scale_x_discrete(name = 'Ecotype') +
+  scale_y_continuous(
+    name = 'Contact Angle (degrees)', 
+  ) +
+  # Style
+  scale_fill_manual(values = c('#514663', '#cacf85')) +
+  theme_minimal() +
+  theme(
+    legend.position = "none",
+    axis.text = element_text(size=12),
+    axis.title = element_text(size=14),
+  )
+contact_ab_boxplot
+
+ggsave(
+  filename = 'Prelim_ab_contact_angle.png', 
+  plot = contact_ab_boxplot,
+  device = 'png',
+  path = './Results/Figures/',
+  scale = 1,
+  width = 3,
+  height = 6,
+  bg = 'white'
+)
+
+t.test(data=filter(contact_angle, leaf_side=="ad"), contact_angle ~ ecotype)
+t.test(data=filter(contact_angle, leaf_side=="ab"), contact_angle ~ ecotype)
+
